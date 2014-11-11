a016d0e53511738fc1de1995285869b36954c7b6
bborges <bborgesr@macalester.edu>
2014-11-11 16:23:47 -0600

    started js

--- moodism.js
+++ moodism.js
@@ -1,1 +1,19 @@
 //javascript file
+// This is jquery's way of saying document.addEventListener("DOMContentLoaded", function(){})
+// See http://learn.jquery.com/using-jquery-core/document-ready/ for more description
+$(document).ready(function() {
+    var answers = {};
+
+//    if($('input:radio:checked').length > 0){
+//        $(this).css( "color", "red" );
+//        $( ".continue" ).show();
+//    }
+
+//    $( ".textAnswer" ).click( function( event ) {
+//        $(this).css( "color", "red" );
+//        $( ".continue" ).show();
+//    });
+
+    $( "form" ).submit(function( event ) {
+    });
+});
